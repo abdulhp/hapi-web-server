00f7d67333528ac04a0e4fa13d825662ed7de600
abdulhp <hpabdul902@gmail.com>
2023-03-24 14:36:50 +0700

    Method Request and Routing

---
+++ routes.js
@@ -0,0 +1,40 @@
+const routes = [
+	{
+		method: '*',
+		path: '/',
+		handler: (request, h) => {
+			return 'Halaman tidak dapat diakses dengan method tersebut';
+		}
+	},
+	{
+		method: 'GET',
+		path: '/',
+		handler: (request, h) => {
+			return 'Homepage';
+		}
+	},
+	{
+		method: 'GET',
+		path: '/about',
+		handler: (request, h) => {
+			return 'About page';
+		}
+	},
+	{
+		method: '*',
+		path: '/about',
+		handler: (request, h) => {
+			return 'Halaman tidak dapat diakses dengan method tersebut';
+		}
+	},
+	{
+		method: '*',
+		path: '/{any*}',
+		handler: (request, h) => {
+			return 'Halaman tidak ditemukan!';
+		}
+	},
+];
+
+module.exports = routes;
+
